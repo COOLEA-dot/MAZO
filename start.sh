92c75fd0c18a476438e9e5fc1bad16cd35ba32fa
COOLEA-dot <nicolauolea@gmail.com>
2025-05-27 13:42:47 +0200

    Cambio en el start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Crear carpeta si no existe
+# Crear carpeta contenedora si no existe
 mkdir -p static/uploads
 
-# Crear el enlace simbólico si aún no existe
+# Eliminar carpeta incorrecta si ya existe
+if [ -d static/uploads/videos ] && [ ! -L static/uploads/videos ]; then
+  rm -rf static/uploads/videos
+fi
+
+# Crear el enlace simbólico si no existe
 if [ ! -L static/uploads/videos ]; then
   ln -s /mnt/videos static/uploads/videos
 fi
